acbc30fb813c1e2b4fe524ef7ddd9f2cef984bb5
Jose Ribeiro <joseribeirocsr@gmail.com>
2025-07-29 15:49:56 -0300

    validação das sessão e exibição das mensagens.

--- sp-tech-desenvolvimento-back-end/aprenda-a-criar-formularios-com-condicionais-e-sessoes-com-php/script.php
+++ sp-tech-desenvolvimento-back-end/aprenda-a-criar-formularios-com-condicionais-e-sessoes-com-php/script.php
@@ -1,30 +1,13 @@
 <?php
-session_start();
+
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php";
+include "servicos/servicoCategoriaCompetidor.php";
+
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-if (empty($nome)) {
-    $_SESSION['mensagem de erro'] = 'O nome não pode estar vazio';
-    header('location: form.php');
-    return;
-} else if (strlen($nome) < 2) {
-    $_SESSION['mensagem de erro'] = 'O nome deve conter no minino 2 caracteres';
-    header('location: form.php');
-    return;
-} else if (strlen($nome) > 40) {
-    $_SESSION['mensagem de erro'] = 'O nome é muito extenso';
-    header('location: form.php');
-    return;
-} else if (!is_numeric($idade)) {
-    $_SESSION['mensagem de erro'] = $idade . " não é valido para idade";
-    header('location: form.php');
-    return;
-}
+defineCategoriaCompetidor($nome, $idade);
 
-
-
-var_dump([
-    $nome,
-    $idade
-]);
+header('location: index.php');
